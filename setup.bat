8c81e78f07cbd66e11079dcb0ed9fd7a06134a58
Bradleym <bradley.milne.1@gmail.com>
2025-03-05 19:44:59 +0000

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -11,7 +11,9 @@ call py -m pip install --upgrade pip
 call py -m pip install numpy
 call py -m pip install scipy
 call py -m pip install matplotlib
+call py -m pip install sympy
 
 call py -m pip install --upgrade numpy
 call py -m pip install --upgrade scipy
 call py -m pip install --upgrade matplotlib
+call py -m pip install --upgrade sympy

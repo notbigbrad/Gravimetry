1f4c0a988d2a587d64fd2a884e7f7e0a1fc6b02d
Bradleym <bradley.milne.1@gmail.com>
2025-03-06 01:13:22 +0000

    ODE fitted, and comparison with math model inputed

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ rawData.append(np.loadtxt(f'./data/15v1.txt', delimiter=",", encoding="utf-8-sig
 rawData.append(np.loadtxt(f'./data/15v2.txt', delimiter=",", encoding="utf-8-sig")[0:-90].T)
 rawData.append(np.loadtxt(f'./data/1v1.txt', delimiter=",", encoding="utf-8-sig")[0:-210].T)
 rawData.append(np.loadtxt(f'./data/1v2.txt', delimiter=",", encoding="utf-8-sig")[200:-10].T)
+# rawData.append(np.loadtxt(f'./data/2v1.txt', delimiter=",", encoding="utf-8-sig")[75:-310].T) # redo for ODEs
 
 positionData = []
 
@@ -127,75 +128,93 @@ for i in range(len(positionData)):
 # ============================================================================= Calculate constants with error
 
 m = 0.109
+mStd = 0.002
 r0 = np.array(l)
-I0 = (2/5)*m*(dBall/2)**2 + m*r0**2
+r0Std = lStd
+I0 = (2/5)*m*(dBall/2)**2 + m*np.array(l)**2
+I0Std = 0
 
 # ============================================================================= Fit model to data
 
 g = []
 gStd = []
 
+gODE = []
+gStdODE = []
+
 for i in range(len(angularData)):
 
     time, theta = angularData[i]
         
     # Initial guesses
-    # I = [0.03, 0.1, np.sqrt(9.81/l[i]), np.pi/4] # A0, gamma, omega, phi
-    I = [0, 0, 9.816/4, 0.1] # thet0, om0, g, b ODE
+    I1 = [0.03, 0.1, np.sqrt(9.81/l[i]), np.pi/4] # A0, gamma, omega, phi
+    I2 = [np.pi/4, 0, 9.816, 0.0001] # thet0, om0, g, b ODE
 
     # Bounds
-    # bounds = [[0.0001,0.0001,0.1,-np.pi],[0.04,1,10,np.pi]] # bounds on the fitting function
-    bounds = [[-np.pi,0,9.80/4,0.001],[np.pi,2,9.83/4,0.1]] # bounds on the fitting function ODE
-
-    constants = [m, r0[i], I0[i]] # m, r, I
+    bounds1 = [[0.0001,0.0001,0.1,-np.pi],[0.04,1,10,np.pi]] # bounds on the fitting function
+    bounds2 = [[-np.pi,0,9.7,0.00001],[np.pi,5,9.9,0.1]] # bounds on the fitting function ODE
 
     # Fit model
-    # optimal, covariance = scipy.optimize.curve_fit(physicalPendulum, time, theta, p0=I, bounds=bounds, maxfev=1*10**9)
-    optimal, covariance = scipy.optimize.curve_fit(lambda t, thet0, om0, g, b : physicalODE(t, thet0, om0, g, b, const=constants), time, theta, p0=I, bounds=bounds, maxfev=1*10**9)
+    optimal1, covariance1 = scipy.optimize.curve_fit(physicalPendulum, time, theta, p0=I1, bounds=bounds1, maxfev=1*10**9)
+    optimal2, covariance2 = scipy.optimize.curve_fit(lambda t, thet0, om0, g, b : physicalODE(t, thet0, om0, g, b, m=m, r=r0[i], I=I0[i]), time, theta, p0=I2, bounds=bounds2, maxfev=1*10**9)
 
-    # Find natural frequency
-    # o0 = np.sqrt(optimal[2]**2 + optimal[1]**2)
-    # o0Std = sqrt(o0,optimal[2],optimal[1],np.sqrt(covariance[2,2]),np.sqrt(covariance[1,1]),0, "+")
-    # o0 = optimal[2]
-    # o0Std = np.sqrt(covariance[2,2])
+    o = optimal1[2]
+    oStd = np.sqrt(covariance1[2,2])
+    gamma = optimal1[1]
+    gammaStd = np.sqrt(covariance1[1,1])
 
-    # # Calculate g
-    # o0Sym, lSym = sp.symbols('o0Sym, lSym')
-    # expr_g = (o0Sym**2)*lSym
+    # Calculate g
+    oSym, gammaSym, lSym = sp.symbols('o0Sym, gammaSym, lSym')
+    expr_g = (oSym**2 + gammaSym**2)*lSym
 
-    # g_curr, g_std = evaluation_with_error(
-    #     my_function=expr_g,
-    #     omega0=[(o0,o0Std),o0Sym],
-    #     length=[(l[i],lStd[i]),lSym],
-    # )
+    g_curr, g_std = evaluation_with_error(
+        my_function=expr_g,
+        omega=[(o,oStd),oSym],
+        gamma=[(gamma,gammaStd),gammaSym],
+        length=[(l[i],lStd[i]),lSym],
+    )
     
-    # g.append(g_curr)
-    # gStd.append(g_std)
+    g.append(g_curr)
+    gStd.append(g_std)
     
-    g.append(optimal[2])
-    gStd.append(np.sqrt(covariance[2,2]))
+    gODE.append(optimal2[2])
+    gStdODE.append(np.sqrt(covariance2[2,2]))
 
-    print(f'g: {g[i]:.5f} +- {gStd[i]:.5f} ms^-2')
+    print(f'g: {g[i]:.5f} +- {gStd[i]:.5f} ms^-2 (math)')
+    print(f'g: {gODE[i]:.5f} +- {gStdODE[i]:.5f} ms^-2 (phys)')
 
     # Calculate residuals
-    r = theta - physicalPendulum(np.linspace(np.min(time),np.max(time),len(time)), optimal[0], optimal[1], optimal[2], optimal[3])
+    r1 = theta - physicalPendulum(np.linspace(np.min(time),np.max(time),len(time)), optimal1[0], optimal1[1], optimal1[2], optimal1[3])
+    r2 = theta - physicalODE(np.linspace(np.min(time),np.max(time),len(time)), optimal2[0], optimal2[1], optimal2[2], optimal2[3], m, r0[i], I0[i])
 
     # Plot output
     tSpace = np.linspace(np.min(time),np.max(time),10000)
-    plt.figure(figsize=[10,7])
-    plt.suptitle("plt")
+    plt.figure(figsize=[14,7])
+    plt.suptitle("Data Plot with Fitted Model")
     plt.axis("off")
-    plt.subplot(211)
-    plt.title("Data Plot with Fitted Model")
+    plt.subplot(321)
+    plt.title("Mathematical Pendulum Model")
     plt.fill_between(time, theta-0, theta+0, color="lightgray")
     plt.scatter(time, theta, color="red", marker="+", linewidths=1, label="Data")
-    plt.plot(tSpace, physicalPendulum(tSpace, optimal[0], optimal[1], optimal[2], optimal[3]), label="Mathematical Pendulum Model")
-    plt.plot(tSpace, max(theta)*sin(tSpace, np.sqrt(9.81/l[i]), 0), "g--", label="Theoretical")
-    plt.legend()
-    plt.subplot(212)
-    plt.title("Residual")
+    plt.plot(tSpace, physicalPendulum(tSpace, optimal1[0], optimal1[1], optimal1[2], optimal1[3]), label="Mathematical Pendulum Model")
+    # plt.plot(tSpace, max(theta)*sin(tSpace, np.sqrt(9.81/l[i]), 0), "g--", label="Theoretical")
+    plt.subplot(322)
+    plt.title("Physical Pendulum Model")
+    plt.fill_between(time, theta-0, theta+0, color="lightgray")
+    plt.scatter(time, theta, color="red", marker="+", linewidths=1, label="Data")
+    plt.plot(tSpace, physicalODE(tSpace, optimal2[0], optimal2[1], optimal2[2], optimal2[3], m, r0[i], I0[i]), label="Physical Pendulum Model")
+    # plt.plot(tSpace, max(theta)*sin(tSpace, np.sqrt(9.81/l[i]), 0), "g--", label="Theoretical")
+    plt.subplot(323)
+    plt.title("Residual (Math)")
     plt.plot(time, theta*0.05, color="lightgray")
-    plt.plot(time, r, color="black")
+    plt.plot(time, r1, color="black")
+    plt.subplot(324)
+    plt.title("Residual (Phys)")
+    plt.plot(time, theta*0.05, color="lightgray")
+    plt.plot(time, r2, color="black")
+    plt.subplot(325)
+    plt.title("Residual (Math-Phys)")
+    plt.plot(r1-r2, color="black")
     plt.show()
 
 # ============================================================================= Propogate errors and find final value
@@ -204,5 +223,9 @@ q = np.linspace(0, len(g) - 1, len(g))
 optimal, covariance = scipy.optimize.curve_fit(f, q, g, p0=[9.81616], sigma=gStd, maxfev=1*10**9)
 GStd = np.sqrt(covariance[0,0])
 G = optimal[0]
+optimal, covariance = scipy.optimize.curve_fit(f, q, gODE, p0=[9.81616], sigma=gStdODE, maxfev=1*10**9)
+GStdODE = np.sqrt(covariance[0,0])
+GODE = optimal[0]
 
-print(f'Final value of g using all experiments: {G:.5f} +- {GStd:.5f} ms^-2')
+print(f'Final value of g using all experiments: {G:.5f} +- {GStd:.5f} ms^-2 (math)')
+print(f'Final value of g using all experiments: {GODE:.5f} +- {GStdODE:.5f} ms^-2 (phys)')
